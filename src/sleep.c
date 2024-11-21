796c2acbe7f088634caca0082116ebb507542f3b
Jared <jarednay91@gmail.com>
2024-11-21 16:00:12 -0700

    init commit, added sleep.c task_delay.c and timer.c

---
+++ src/sleep.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+int main(void)
+{
+    int toggle = 0;
+    stdio_init_all();
+    gpio_init(OUT_PIN);
+    gpio_set_dir(OUT_PIN, GPIO_OUT);
+    gpio_put(OUT_PIN, toggle);
+
+    while (true) {
+        toggle = !toggle;
+        gpio_put(OUT_PIN, toggle);
+        sleep_ms(DELAY_MS);
+    }
+}
